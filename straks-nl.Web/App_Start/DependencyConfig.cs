ea077dcdb42d02bb4473fbe94c0e5974b1e8572d
PC-LARS\LarsV <larsvolkers@hotmail.nl>
2020-07-10 16:52:48 +0200

    Add AutoMapper with AutoFac support

--- straks-nl.Web/App_Start/DependencyConfig.cs
+++ straks-nl.Web/App_Start/DependencyConfig.cs
@@ -2,6 +2,7 @@
 using Autofac.Integration.Mvc;
 using Microsoft.Ajax.Utilities;
 using Owin;
+using straks_nl.Web.Utils.AutoMapper;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,13 +17,13 @@ namespace straks_nl.Web.App_Start
         public static void RegisterDependencyInjection(this IAppBuilder app, Action<ContainerBuilder> registerDependencies)
         {
             var builder = new ContainerBuilder();
-
+            var assemblies = typeof(MvcApplication).Assembly;
             // Register your MVC controllers. (MvcApplication is the name of
             // the class in Global.asax.)
-            builder.RegisterControllers(typeof(MvcApplication).Assembly);
+            builder.RegisterControllers(assemblies);
 
             // OPTIONAL: Register model binders that require DI.
-            builder.RegisterModelBinders(typeof(MvcApplication).Assembly);
+            builder.RegisterModelBinders(assemblies);
             builder.RegisterModelBinderProvider();
 
             // OPTIONAL: Register web abstractions like HttpContextBase.
@@ -30,6 +31,9 @@ namespace straks_nl.Web.App_Start
 
             // OPTIONAL: Enable property injection in view pages.
             builder.RegisterSource(new ViewRegistrationSource());
+
+            //Enable AutoMapper injection
+            builder.RegisterModule(new AutoMapperModule(assemblies));
 
             registerDependencies(builder);
             // Set the dependency resolver to be Autofac.
---
+++ straks-nl.Web/Utils/AutoMapper/SharedMappingProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace straks_nl.Web.Utils.AutoMapper
+{
+    public class SharedMappingProfile : Profile
+    {
+        public SharedMappingProfile()
+        {
+            //Add mappings here
+        }
+    }
+}
